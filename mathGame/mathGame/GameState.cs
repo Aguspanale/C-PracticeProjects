5bb57e9262b8194d12d106507c6f5e48f47667a3
Agustín Pañale <apanale@dc.uba.ar>
2024-05-11 17:09:53 -0300

    MVP finished. Next in line is to make it not generate invalid divisions, write some integration tests and finally implement the result logging.

---
+++ mathGame/mathGame/GameState.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal interface IGameState
+    {
+        public bool IsWin(Game aGame);
+        public void declareResults(Menu menu, Game aGame);
+    }
+
+    internal class UnfinishedGameState : IGameState
+    {
+
+        public bool IsWin(Game aGame)
+        {
+            return aGame.IsWinWhenUnfinished();
+        }
+        public void declareResults(Menu menu, Game aGame)
+        {
+            aGame.IsWinWhenUnfinished();
+        }
+    }
+    internal abstract class FinishedGameState : IGameState
+    {
+        public abstract bool IsWin(Game aGame);
+        public abstract void declareResults(Menu menu,Game aGame);
+        internal static FinishedGameState representResult(bool gameWasWon)
+        {
+             if(gameWasWon)
+            {
+                return new WonGameState();
+            }
+            return new LostGameState();
+        }
+        
+    }
+    internal class WonGameState : FinishedGameState
+    {
+        public override bool IsWin(Game aGame)
+        {
+            return true;
+        }
+        public override void declareResults(Menu aMenu, Game aGame)
+        {
+            aMenu.DeclareGameWon();
+        }
+    }
+    internal class LostGameState : FinishedGameState
+    {
+        public override bool IsWin(Game aGame)
+        {
+            return false;
+        }
+        public override void declareResults(Menu aMenu, Game aGame)
+        {
+            aMenu.DeclareGameLost(aGame);
+        }
+    }
+}
+
