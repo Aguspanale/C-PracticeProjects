828cf5c0f559b5747158dd1e656673932f5e8778
Agustín Pañale <apanale@dc.uba.ar>
2024-05-13 12:59:12 -0300

    Started new project "HabitLogger" to practice CRUD operations in c# using the SQLite connection provided by ADO.NET

---
+++ HabitLogger/HabitLogger/Program.cs
@@ -0,0 +1,57 @@
+﻿// CREATE READ UPDATE DELETE
+
+using Microsoft.Data.Sqlite;
+
+namespace HabitLogger
+{ 
+    public class HabitLogger
+    {
+        public static void Main()
+        {
+            HabitLogger logger = new HabitLogger();
+            logger.connect();           
+        }
+        internal void connect()
+        {
+            using (SqliteConnection connection = new SqliteConnection("Data Source = Habits.db"))
+            {
+                connection.Open();
+                SqliteCommand command = connection.CreateCommand() ;
+                command.CommandText =
+                @"
+                    CREATE TABLE IF NOT EXISTS habits(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Name TEXT NOT NULL                    
+                )";
+                command.ExecuteNonQuery();
+
+
+                command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                    INSERT INTO habits (Name)
+                    VALUES('A Habit!')
+                ";
+                command.ExecuteNonQuery();
+
+                command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                    SELECT *
+                    FROM habits
+                ";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name = reader.GetString(1);
+
+                        Console.WriteLine($"{name}!");
+                    }
+                }
+                connection.Close();
+            }
+        }       
+
+    }
+}
