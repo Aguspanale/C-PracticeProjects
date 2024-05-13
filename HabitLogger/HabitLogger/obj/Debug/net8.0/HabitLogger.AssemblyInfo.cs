c775c9f3bc0ef412ea2177ea84dfd065f313db21
Agustín Pañale <apanale@dc.uba.ar>
2024-05-13 15:09:29 -0300

    Started working on "databaseManager" class. it can currently create the db, insert into it and count apparitions. I need to make it not vulnerable to SQL injections in the future

--- HabitLogger/HabitLogger/obj/Debug/net8.0/HabitLogger.AssemblyInfo.cs
+++ HabitLogger/HabitLogger/obj/Debug/net8.0/HabitLogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697e73bc910bc2df460aa15bb42565b539bef35e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
